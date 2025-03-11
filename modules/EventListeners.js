f4eb91262e836957a127accaa9a551f7708e0b2b
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-03-11 16:49:48 -0300

    atualizanod codigo

--- modules/EventListeners.js
+++
@@ -1,7 +0,0 @@
-import { handleCalculate } from "./handleCalculate.js";
-import { handleReset } from "./handleReset.js";
-
-export function setupEventListeners() {
-  document.querySelector("#calculate").addEventListener("click", handleCalculate);
-  document.querySelector("#reset").addEventListener("click", handleReset);
-}
